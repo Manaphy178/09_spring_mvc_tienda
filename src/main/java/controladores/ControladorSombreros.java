83e366583cb506ae253d73aebec06442571288e1
jgonzalez <jgonzalez@PCGS32.nelson.local>
2024-10-03 20:01:23 +0200

    03/10/2024

--- src/main/java/controladores/ControladorSombreros.java
+++ src/main/java/controladores/ControladorSombreros.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import daos.SombrerosDAO;
 import modelo.*;
 import java.util.*;
 import daosImpl.SombrerosDAOImpl;
@@ -16,12 +18,17 @@ public class ControladorSombreros {
 	 * 
 	 */
 	@Autowired
-	private SombrerosDAOImpl sombrerosDAO;
+	private SombrerosDAO sombrerosDAO;
 
+	/*
+	 * basicamente asi le hemos dicho a spring que nos de la unica bean que tenga de
+	 * la unica clase que este implementando el interfaz SombrerosDAO
+	 */
+// lo que pongamos en @RequestMapping es la ruta que atiende el siguiente metodo
 	@RequestMapping("admin/sombreros")
 	public String obtenerLibros(Model model) {
 		List<Sombrero> sombreros = sombrerosDAO.obtenerSombrero();
-		model.addAttribute("sombreros",sombreros);
+		model.addAttribute("sombreros", sombreros);
 		return "admin/sombreros";
 	}
 }
